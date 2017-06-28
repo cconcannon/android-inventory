a714c67eca80d48d1d5fcf87f4f4ecb894fa8f04
Chris Concannon <CConcannon@walmartlabs.com>
2017-06-28 16:31:56 -0700

    created Item class

---
+++ app/src/main/java/Item.java
@@ -0,0 +1,39 @@
+/**
+ * Created by vn0mrky on 6/28/17.
+ */
+
+public class Item {
+    private String mName;
+    private String mDescription;
+    private int mQuantity;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Item(String name, String description, int quantity, int imageResourceId) {
+        mName = name;
+        mDescription = description;
+        mQuantity = quantity;
+        mImageResourceId = imageResourceId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public int getQuantity() {
+        return mQuantity;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
