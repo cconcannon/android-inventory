7c6a7a2900bc4f699a00098b3bc455200d9625a5
Chris Concannon <CConcannon@walmartlabs.com>
2017-09-07 12:16:21 -0700

    editor activity populates with stored db information

--- app/src/main/java/com/example/vn0mrky/inventory/AddItemActivity.java
+++ app/src/main/java/com/example/vn0mrky/inventory/AddItemActivity.java
@@ -1,44 +1,69 @@
 package com.example.vn0mrky.inventory;
 
+import android.app.AlertDialog;
+import android.app.LoaderManager;
 import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.net.Uri;
 
 import com.example.vn0mrky.inventory.data.InventoryContract;
 import com.example.vn0mrky.inventory.data.InventoryDbHelper;
 
-public class AddItemActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int EXISTING_ITEM_LOADER = 0;
+    private Uri mCurrentItemUri;
     private EditText mNameEdit;
     private EditText mDescriptionEdit;
     private EditText mQuantityEdit;
     private EditText mPriceEdit;
+    private boolean mEntryChanged = false;
+
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mEntryChanged = true;
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
+        Intent intent = getIntent();
+        mCurrentItemUri = intent.getData();
+
+        if (mCurrentItemUri == null) {
+            setTitle("Add Item to Inventory");
+        } else {
+            setTitle("Edit Existing Item");
+            getLoaderManager().initLoader(EXISTING_ITEM_LOADER, null, this);
+        }
+
         mNameEdit = (EditText) findViewById(R.id.input_item_name);
         mDescriptionEdit = (EditText) findViewById(R.id.input_item_description);
         mQuantityEdit = (EditText) findViewById(R.id.quantity);
         mPriceEdit = (EditText) findViewById(R.id.price_input);
-
-        final Button saveButton = (Button) findViewById(R.id.add_item_button);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                insertItem();
-            }
-        });
     }
 
-    private void insertItem() {
+    private void saveItem() {
         String nameString = mNameEdit.getText().toString().trim();
         String descriptionString = mDescriptionEdit.getText().toString().trim();
         int quantity = Integer.parseInt(mQuantityEdit.getText().toString().trim());
@@ -46,8 +71,6 @@ public class AddItemActivity extends AppCompatActivity {
         int price = Math.round(priceInput*100);
 
         InventoryDbHelper mDbHelper = new InventoryDbHelper(this);
-
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(InventoryContract.InventoryEntry.COLUMN_ITEM_NAME, nameString);
@@ -61,5 +84,51 @@ public class AddItemActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, R.string.successful_entry, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                InventoryContract.InventoryEntry._ID,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_NAME,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_DESCRIPTION,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_QUANTITY,
+                InventoryContract.InventoryEntry.COLUMN_ITEM_PRICE
+        };
+
+        return new CursorLoader(this, mCurrentItemUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data == null || data.getCount() < 1 ) {
+            return;
+        }
+
+        if (data.moveToFirst()) {
+            int nameColumnIndex = data.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_ITEM_NAME);
+            int descriptionColumnIndex = data.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_ITEM_DESCRIPTION);
+            int quantityColumnIndex = data.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_ITEM_QUANTITY);
+            int priceColumnIndex = data.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_ITEM_PRICE);
+
+            String name = data.getString(nameColumnIndex);
+            String description = data.getString(descriptionColumnIndex);
+            int quantity = data.getInt(quantityColumnIndex);
+            int price100 = data.getInt(priceColumnIndex);
+            double price = price100/100;
+
+            mNameEdit.setText(name);
+            mDescriptionEdit.setText(description);
+            mQuantityEdit.setText(Integer.toString(quantity));
+            mPriceEdit.setText(Double.toString(price));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mNameEdit.setText("");
+        mDescriptionEdit.setText("");
+        mPriceEdit.setText("");
+        mQuantityEdit.setText("");
     }
 }
